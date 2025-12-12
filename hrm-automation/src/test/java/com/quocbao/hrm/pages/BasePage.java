c27e428c748a9fc34a4964bd0588891d186c9b09
QuocBao <baovo32911@gmail.com>
2025-12-12 22:38:22 +0700

    add Login Page

---
+++ hrm-automation/src/test/java/com/quocbao/hrm/pages/BasePage.java
@@ -0,0 +1,4 @@
+package com.quocbao.hrm.pages;
+
+public class BasePage {
+}
---
+++ hrm-automation/src/test/java/com/quocbao/hrm/pages/LoginPage.java
@@ -0,0 +1,49 @@
+package com.quocbao.hrm.pages;
+
+import com.quocbao.hrm.helpers.PropertiesHelper;
+import com.quocbao.hrm.keywords.WebUI;
+import org.openqa.selenium.By;
+
+public class LoginPage {
+    private By inputUserName = By.id("iusername");
+    private By inputPassword = By.id("ipassword");
+    private By btnLogin = By.xpath("//button[@type='submit']");
+    private By errorMessage=By.xpath("//div[@class='toast-message' and contains(text(),'Invalid')]");
+
+    private void setUserName(String userName) {
+        WebUI.setText(inputUserName, userName);
+    }
+
+    private void setPassword(String password) {
+        WebUI.setText(inputPassword, password);
+    }
+
+    private void clickButtonLogin() {
+        WebUI.clickElement(btnLogin);
+    }
+
+    public void verifyLoginSuccess() {
+        WebUI.waitForUrlContains("desk");
+        WebUI.assertContains(WebUI.getCurrentURL(), "desk", "FAIL");
+    }
+
+    public void verifyLoginFail(){
+        WebUI.assertContains(WebUI.getCurrentURL(),"login","FAIL");
+        WebUI.assertEquals(WebUI.getText(errorMessage),"Invalid Login Credentials.","FAIL");
+    }
+
+    public void loginHRM(String userName, String password) {
+        WebUI.openURL(PropertiesHelper.getValue("URL"));
+        WebUI.watiForPageLoad();
+
+        WebUI.clearText(inputUserName);
+        setUserName(userName);
+
+        WebUI.clearText(inputPassword);
+        setPassword(password);
+
+        clickButtonLogin();
+        WebUI.watiForPageLoad();
+
+    }
+}
